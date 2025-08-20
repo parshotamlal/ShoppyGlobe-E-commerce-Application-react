ae30e375868f76630ca0a93bcb98dfa0fa0376ba
parshotam lal <145228373+parshotamlal@users.noreply.github.com>
2025-08-21 00:15:55 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import './index.css'
+import './App.css'
 function App() {
   return (
-     <div className="h-screen flex items-center justify-center bg-gradient-to-r from-amber-400 to-amber-600 ml-72">
-      <h1 className="text-4xl font-extrabold text-white drop-shadow-lg hover:scale-110 transition-transform">📚 Hello Library</h1>
+     <div >
+      <h1 className=" bg-black">hello</h1>
     </div>
   )
 }
-
 export default App
 
 
